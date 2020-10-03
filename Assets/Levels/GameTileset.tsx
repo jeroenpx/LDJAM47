d5c31858b4184a50e2de152e7162d9bec582c30e
jeroenpx <jeroenpx@users.noreply.github.com>
2020-10-03 10:50:15 +0200

    Defining internal level representation: first level (tmx file) & tileset

---
+++ Assets/Levels/GameTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="SpriteMap" tilewidth="16" tileheight="16" tilecount="36" columns="6">
+ <image source="SpriteMap.png" width="96" height="96"/>
+</tileset>
